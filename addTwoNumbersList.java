fd0e68f2074f86e3bcdfe2bad9cb7de2b0d957ee
Kritika Parthasarathy <kritikaps11@gmail.com>
2021-03-16 21:12:01 -0400

    Update addTwoNumbersList.java

--- addTwoNumbersList.java
+++ addTwoNumbersList.java
@@ -4,25 +4,25 @@ public class addTwoNumbersList {
 	    
 	    ListNode head = null;
 	    ListNode temp = null;
-	    int carry = 0;
+	    int carry = 0; //if sum is greater than 10, sum%10 is stored here
 	    
-	    while(l1!=null||l2!=null)
+	    while(l1!=null||l2!=null) //runs until end of l1 and l2
 	    {
 	        int sum = carry;
 	        if (l1!=null)
 	        {
-	            sum+=l1.val;
+	            sum+=l1.val; //adds sum to l1 and traverses through l1
 	            l1=l1.next;
 	        }
 	         if (l2!=null)
 	        {
-	            sum+=l2.val;
+	            sum+=l2.val;//adds sum to l2 and traverses through l2
 	            l2=l2.next;
 	        }
 	        
 	        ListNode result = new ListNode (sum%10);
 	        carry = sum/10;
-	        if (temp == null)
+	        if (temp == null)  
 	        {
 	            temp = head = result;
 	        }
