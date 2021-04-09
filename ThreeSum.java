60612035ff9fb83065f92b31a51edaa2b09d9e1d
Kritika Parthasarathy <kritikaps11@gmail.com>
2021-04-08 22:33:04 -0400

    Add files via upload

---
+++ ThreeSum.java
@@ -0,0 +1,25 @@
+package learningjava;
+
+public class ThreeSum {
+	public List<List<Integer>> threeSum(int[] nums) {
+	      List<List<Integer>> list = new ArrayList<List<Integer>>(); 
+	        for (int i=0; i<nums.length;i++){
+	            for (int n=i; i<nums.length;n++)
+	            {
+	                for(int k=n; k<nums.length; k++)
+	                {
+	                    if (i != k && k != n && i != n && (nums[i] + nums[n] + nums[k]) == 0)
+	                    {
+	                        List<Integer> part = new ArrayList<Integer>(3);
+	                        part.add(nums[i]);
+	                        part.add(nums[k]);
+	                        part.add(nums[n]);
+	                        list.add(part);
+	                    }
+	                }
+	            }
+	        }
+	        
+	        return list;
+	    }
+}
